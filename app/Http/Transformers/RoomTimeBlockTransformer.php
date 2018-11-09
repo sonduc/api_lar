bc564252892095782e36da5d49e450304a36cb6c
Unknown <nxh0809@gmail.com>
2018-11-09 11:35:31 +0700

    RoomTimeBlockTransformer with time parsing

--- app/Http/Transformers/RoomTimeBlockTransformer.php
+++ app/Http/Transformers/RoomTimeBlockTransformer.php
@@ -3,14 +3,21 @@
 namespace App\Http\Transformers;
 
 use App\Repositories\Rooms\RoomTimeBlock;
+use Carbon\Carbon;
 use League\Fractal\TransformerAbstract;
 
 class RoomTimeBlockTransformer extends TransformerAbstract
 {
-    protected $availableIncludes
-        = [
+    protected $availableIncludes = [
 
-        ];
+    ];
+
+    private $now;
+
+    public function __construct()
+    {
+        $this->now = Carbon::now()->startOfDay();
+    }
 
     public function transform(RoomTimeBlock $room = null)
     {
@@ -18,10 +25,27 @@ class RoomTimeBlockTransformer extends TransformerAbstract
             return [];
         }
 
+        $room->date_start = $this->checkDate($room->date_start);
+        $room->date_end   = $this->checkDate($room->date_end);
+
         return [
             'date_start' => $room->date_start,
             'date_end'   => $room->date_end,
         ];
+    }
+
+    /**
+     * Chuyển ngày về tương lai
+     * @author HarikiRito <nxh0809@gmail.com>
+     *
+     * @param $date
+     *
+     * @return string
+     */
+    private function checkDate($date)
+    {
+        $c_date = Carbon::parse($date)->startOfDay()->timestamp;
+        return ($c_date <= $this->now->copy()->timestamp) ? $this->now->toDateString() : $date;
     }
 
 }
