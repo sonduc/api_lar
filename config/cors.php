2a9a00230dbf53701522029fe4bfd793fc107ecb
Pham Tuan Anh <tuananhpham1402@gmail.com>
2018-12-01 11:59:41 +0700

    test cors

--- config/cors.php
+++ config/cors.php
@@ -10,10 +10,10 @@ return [
     | to accept any value.
     |
     */
-    'supportsCredentials' => true,
+    'supportsCredentials' => false,
     'allowedOrigins' => ['*'],
     'allowedHeaders' => ['*'],
     'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
     'exposedHeaders' => [],
-    'maxAge' => 0,
+    'maxAge' => 86400,
 ];
