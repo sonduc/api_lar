074737e9067543a06c222c1dd3079da24693c10f
ducchien0612 <ducchien0612@gmail.com>
2019-01-08 14:15:52 +0700

    fix-api-baokim

--- app/Http/Controllers/ApiCustomer/BookingController.php
+++ app/Http/Controllers/ApiCustomer/BookingController.php
@@ -31,100 +31,101 @@ use phpDocumentor\Reflection\DocBlockFactory;
 class BookingController extends ApiController
 {
     protected $validationRules    = [
-        'name'             => 'required|v_title',
-        'name_received'    => 'nullable|v_title',
-        'phone'            => 'required|between:10,14|regex:/^\+?[0-9-]*$/',
-        'phone_received'   => 'nullable|between:10,14|regex:/^\+?[0-9-]*$/',
-        'sex'              => 'nullable|integer|between:0,3',
-        'birthday'         => 'nullable|date_format:Y-m-d',
-        'email'            => 'required|email',
-        'email_received'   => 'nullable|email',
-        'room_id'          => 'required|integer|exists:rooms,id,deleted_at,NULL',
-        'staff_id'         => 'nullable|integer|exists:users,id,deleted_at,NULL',
-        'staff_note'       => 'nullable|v_title',
-        'checkin'          => 'required|date|after:now',
-        'checkout'         => 'required|date|after:checkin',
-        'additional_fee'   => 'filled|integer|min:0',
-        'price_discount'   => 'filled|integer|min:0',
-        'coupon'           => 'nullable|string',
-        'note'             => 'nullable|v_title',
-        'number_of_guests' => 'bail|required|guest_check|integer|min:1',
-        'customer_id'      => 'nullable|integer|exists:users,id,deleted_at,NULL',
-        'status'           => 'nullable|in:1',
-        'type'             =>  'required|in:2',
-        'booking_type'     => 'bail|required|integer|between:1,2|booking_type_check',
-        // 'payment_method'   => 'required|in:2,3,4,5',
-        // 'payment_status'   => 'required|in:0',
-        'source'           => 'required|in:4',
-        'exchange_rate'    => 'nullable|integer',
-        'money_received'   => 'integer|filled|min:0',
-        'confirm'          => 'integer|between:0,1',
+        'name'                      => 'required|v_title',
+        'name_received'             => 'nullable|v_title',
+        'phone'                     => 'required|between:10,14|regex:/^\+?[0-9-]*$/',
+        'phone_received'            => 'nullable|between:10,14|regex:/^\+?[0-9-]*$/',
+        'sex'                       => 'nullable|integer|between:0,3',
+        'birthday'                  => 'nullable|date_format:Y-m-d',
+        'email'                     => 'required|email',
+        'email_received'            => 'nullable|email',
+        'room_id'                   => 'required|integer|exists:rooms,id,deleted_at,NULL',
+        'staff_id'                  => 'nullable|integer|exists:users,id,deleted_at,NULL',
+        'staff_note'                => 'nullable|v_title',
+        'checkin'                   => 'required|date|after:now',
+        'checkout'                  => 'required|date|after:checkin',
+        'additional_fee'            => 'filled|integer|min:0',
+        'price_discount'            => 'filled|integer|min:0',
+        'coupon'                    => 'nullable|string',
+        'note'                      => 'nullable|v_title',
+        'number_of_guests'          => 'bail|required|guest_check|integer|min:1',
+        'customer_id'               => 'nullable|integer|exists:users,id,deleted_at,NULL',
+        'status'                    => 'nullable|in:1',
+        'type'                      =>  'required|in:2',
+        'booking_type'              => 'bail|required|integer|between:1,2|booking_type_check',
+        'payment_method'            => 'required|in:4,5',
+        'bank_payment_method_id'    => 'required|integer',
+        'source'                    => 'required|in:4',
+        'exchange_rate'             => 'nullable|integer',
+        'money_received'            => 'integer|filled|min:0',
+        'confirm'                   => 'integer|between:0,1',
     ];
     protected $validationMessages = [
-        'name.required'             => 'Vui lòng điền tên',
-        'name.v_title'              => 'Tên không đúng định dạng',
-        'phone.required'            => 'Vui lòng điền số điện thoại',
-        'phone.between'             => 'Số điện thoại không phù hợp',
-        'phone.regex'               => 'Số điện thoại không hợp lệ',
-        'phone_received.regex'      => 'Số điện thoại không hợp lệ',
-        'sex.integer'               => 'Mã giới tính phải là kiểu số',
-        'sex.between'               => 'Giới tính không phù hợp',
-        'birthday.date_format'      => 'Ngày sinh phải ở định dạng Y-m-d',
-        'email.email'               => 'Email không đúng định dạng',
-        'email.required'            => 'Vui lòng điền địa chỉ email',
-        'room_id.required'          => 'Vui lòng chọn phòng',
-        'room_id.integer'           => 'Mã phòng phải là kiểu số',
-        'room_id.exists'            => 'Phòng không tồn tại',
-        'customer_id.required'      => 'Vui lòng chọn khách hàng',
-        'customer_id.integer'       => 'Mã khách hàng phải là kiểu số',
-        'staff_id.integer'          => 'Mã nhân viên phải là kiểu số',
-        'staff_id.exists'           => 'Nhân viên không tồn tại',
-        'staff_note.v_title'        => 'Phải là văn bản tiếng việt',
-        'checkin.required'          => 'Vui lòng nhập thời gian checkin',
-        'checkin.date_format'       => 'Checkin phải có định dạng Y-m-d H:i:s',
-        'checkin.after'             => 'Thời gian checkin không được phép ở thời điểm quá khứ',
-        'checkout.required'         => 'Vui lòng nhập thời gian checkout',
-        'checkout.date_format'      => 'Checkout phải có định dạng Y-m-d H:i:s',
-        'checkout.after'            => 'Thời gian checkout phải sau thời gian checkin',
-        'additional_fee.required'   => 'Vui lòng điền giá',
-        'additional_fee.filled'     => 'Vui lòng điền giá',
-        'additional_fee.integer'    => 'Giá phải là kiểu số',
-        'price_discount.required'   => 'Vui lòng điền giá',
-        'price_discount.filled'     => 'Vui lòng điền giá',
-        'price_discount.integer'    => 'Giá phải là kiểu số',
-        'coupon.string'             => 'Coupon không được chứa ký tự đặc biệt',
-        'note.v_title'              => 'Note phải là văn bản không chứa ký tự đặc biệt',
-        'number_of_guests.required' => 'Vui lòng điền số khách',
-        'number_of_guests.integer'  => 'Số khách phải là kiểu số',
-        'number_of_guests.min'      => 'Tối thiểu 1 khách',
-        'status.in'                 => 'Mã trạng thái không phù hợp',
-        'status.required'           => 'Trường này không được để trống',
+        'name.required'                         => 'Vui lòng điền tên',
+        'name.v_title'                          => 'Tên không đúng định dạng',
+        'phone.required'                        => 'Vui lòng điền số điện thoại',
+        'phone.between'                         => 'Số điện thoại không phù hợp',
+        'phone.regex'                           => 'Số điện thoại không hợp lệ',
+        'phone_received.regex'                  => 'Số điện thoại không hợp lệ',
+        'sex.integer'                           => 'Mã giới tính phải là kiểu số',
+        'sex.between'                           => 'Giới tính không phù hợp',
+        'birthday.date_format'                  => 'Ngày sinh phải ở định dạng Y-m-d',
+        'email.email'                           => 'Email không đúng định dạng',
+        'email.required'                        => 'Vui lòng điền địa chỉ email',
+        'room_id.required'                      => 'Vui lòng chọn phòng',
+        'room_id.integer'                       => 'Mã phòng phải là kiểu số',
+        'room_id.exists'                        => 'Phòng không tồn tại',
+        'customer_id.required'                  => 'Vui lòng chọn khách hàng',
+        'customer_id.integer'                   => 'Mã khách hàng phải là kiểu số',
+        'staff_id.integer'                      => 'Mã nhân viên phải là kiểu số',
+        'staff_id.exists'                       => 'Nhân viên không tồn tại',
+        'staff_note.v_title'                    => 'Phải là văn bản tiếng việt',
+        'checkin.required'                      => 'Vui lòng nhập thời gian checkin',
+        'checkin.date_format'                   => 'Checkin phải có định dạng Y-m-d H:i:s',
+        'checkin.after'                         => 'Thời gian checkin không được phép ở thời điểm quá khứ',
+        'checkout.required'                     => 'Vui lòng nhập thời gian checkout',
+        'checkout.date_format'                  => 'Checkout phải có định dạng Y-m-d H:i:s',
+        'checkout.after'                        => 'Thời gian checkout phải sau thời gian checkin',
+        'additional_fee.required'               => 'Vui lòng điền giá',
+        'additional_fee.filled'                 => 'Vui lòng điền giá',
+        'additional_fee.integer'                => 'Giá phải là kiểu số',
+        'price_discount.required'               => 'Vui lòng điền giá',
+        'price_discount.filled'                 => 'Vui lòng điền giá',
+        'price_discount.integer'                => 'Giá phải là kiểu số',
+        'coupon.string'                         => 'Coupon không được chứa ký tự đặc biệt',
+        'note.v_title'                          => 'Note phải là văn bản không chứa ký tự đặc biệt',
+        'number_of_guests.required'             => 'Vui lòng điền số khách',
+        'number_of_guests.integer'              => 'Số khách phải là kiểu số',
+        'number_of_guests.min'                  => 'Tối thiểu 1 khách',
+        'status.in'                             => 'Mã trạng thái không phù hợp',
+        'status.required'                       => 'Trường này không được để trống',
 
-        'type.required'              => 'Vui lòng chọn hình thức booking',
-        'type.in'                    => 'Mã hình thức không hợp lệ',
+        'type.required'                         => 'Vui lòng chọn hình thức booking',
+        'type.in'                               => 'Mã hình thức không hợp lệ',
 
-        'booking_type.required'     => 'Vui lòng chọn kiểu booking',
-        'booking_type.integer'      => 'Mã kiểu phải là kiểu số',
-        'booking_type.between'      => 'Mã kiểu không hợp lệ',
+        'booking_type.required'                 => 'Vui lòng chọn kiểu booking',
+        'booking_type.integer'                  => 'Mã kiểu phải là kiểu số',
+        'booking_type.between'                  => 'Mã kiểu không hợp lệ',
 
-        // 'payment_method.in'         => 'Mã hình thức thanh toán không hợp lệ',
-        // 'payment_method.required'   => 'Vui lòng chọn kiểu thanh toán',
+         'payment_method.in'                    => 'Hình thức thanh toán không hợp lệ',
+         'payment_method.required'              => 'Vui lòng chọn kiểu thanh toán',
+         'bank_payment_method_id.required'      => 'Vui lòng chọn ngân hàng thanh toán',
+         'bank_payment_method_id.integer'       => 'Mã ngân hàng không phải là kiểu số',
 
-        // 'payment_status.in'        => "Trạng thái thanh toán không hợp lệ",
-        // 'payment_status.required'  => "Trạng thái thanh toán không được để trống",
 
-        'source.required'           => 'Vui lòng chọn nguồn booking',
-        'source.in'                 => 'Mã nguồn booking không hợp lệ',
-        'exchange_rate.integer'     => 'Tỉ giá chuyển đổi phải là kiểu số',
 
-        'money_received.integer' => 'Tiền nhận phải là kiểu số',
-        'money_received.filled'  => 'Tiền nhận không được để trống',
-        'confirm.required'       => 'Vui lòng chọn trạng thái xác nhận',
-        'confirm.integer'        => 'Mã trạng thái xác nhận phải là kiểu số',
-        'confirm.between'        => 'Trạng thái xác nhận không hợp lệ',
-        'code.integer'           => 'Mã phải là kiểu số',
-        'code.in'                => 'Mã không hợp lệ',
-        'code.required'          => 'Vui lòng chọn lý do',
+        'source.required'                       => 'Vui lòng chọn nguồn booking',
+        'source.in'                             => 'Mã nguồn booking không hợp lệ',
+        'exchange_rate.integer'                 => 'Tỉ giá chuyển đổi phải là kiểu số',
+
+        'money_received.integer'                => 'Tiền nhận phải là kiểu số',
+        'money_received.filled'                 => 'Tiền nhận không được để trống',
+        'confirm.required'                      => 'Vui lòng chọn trạng thái xác nhận',
+        'confirm.integer'                       => 'Mã trạng thái xác nhận phải là kiểu số',
+        'confirm.between'                       => 'Trạng thái xác nhận không hợp lệ',
+        'code.integer'                          => 'Mã phải là kiểu số',
+        'code.in'                               => 'Mã không hợp lệ',
+        'code.required'                         => 'Vui lòng chọn lý do',
     ];
 
     protected $browser;
@@ -495,8 +496,13 @@ class BookingController extends ApiController
      */
     public function payment($uuid, Request $request)
     {
-        $payment_method    = (int) $request->get('payment_method');
         try {
+            $validate            = array_only($this->validationRules, [
+                'payment_method','bank_payment_method_id'
+            ]);
+            $this->validate($request, $validate, $this->validationMessages);
+
+            $payment_method    = (int) $request->get('payment_method');
             $result  = $this->bookingRepository->getBookingByUuid($uuid);
             if (empty($result))
             {
@@ -532,9 +538,12 @@ class BookingController extends ApiController
                     return redirect($baokim_url);
                 }
             }
-        } catch (\Exception $e) {
-            DB::rollback();
-            return $e->getMessage();
+        }catch (\Illuminate\Validation\ValidationException $validationException) {
+            DB::rollBack();
+            return $this->errorResponse([
+                'errors' => $validationException->validator->errors(),
+                'exception' => $validationException->getMessage(),
+            ]);
         }
     }
 }
