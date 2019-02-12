b0cf9a43e480379b01f0e2983be8d76dbb16345a
Pham Tuan Anh <tuananhpham1402@gmail.com>
2019-02-12 14:01:24 +0700

    fix payment fail page

--- app/Http/Controllers/ApiCustomer/PaymentHistoryController.php
+++ app/Http/Controllers/ApiCustomer/PaymentHistoryController.php
@@ -88,7 +88,6 @@ class PaymentHistoryController extends ApiController
                 event(new BookingEvent($booking));
                 event(new CreateBookingTransactionEvent($booking));
                 return redirect(env('PAYMENT_FAIL_REDIRECT'));
-)
                 // return response()->json(['message' => 'Cám ơn bạn đã sử dụng dich vụ của WESTAY']);
             }
         } catch (\Illuminate\Validation\ValidationException $validationException) {
